056c65881c998a5d98ee5a8c3ca8843fd4f7e58a
Alfredo-Azamar <a01798100@tec.mx>
2024-09-12 12:14:34 -0600

    Change to input data

--- server.py
+++ server.py
@@ -21,7 +21,7 @@ def predictJSON():
     data = request.json
     print(data)
     inputData = np.array([
-        int(data['HomePlanet']),
+        data['HomePlanet'],
         data['CryoSleep'],
         data['Destination'],
         data['Age'],
